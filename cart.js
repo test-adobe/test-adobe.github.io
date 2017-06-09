ab3564abdcf6884b8f551a9a22922ce9e8fd582e
test-adobe <hravi@adobe.com>
2017-06-08 17:38:08 -0700

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,19 @@
+digitalData = {}
+
+digitalData = {
+ 
+                tripInfo:{
+                                revenue: "20",
+                                orderID: "756842",
+                                countryDestination : "USA",
+                                countryResidence : "Canada",
+                                durationStay: "25",
+                                tripInfo: "abcd",
+                                tripStart: "abdc",
+                                tripEnd: "cdga",
+                                numberofTravelers: "50",
+                                sportsInsurance: "yes",
+                                channelSource: "marketing",
+                                destinations: "Bali"
+                }
+};
