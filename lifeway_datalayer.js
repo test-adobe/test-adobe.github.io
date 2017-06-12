fa7de5e6650de6acb167c17ccaa0fe43df04099b
test-adobe <hravi@adobe.com>
2017-06-11 22:48:50 -0700

    Create lifeway_datalayer.js

---
+++ lifeway_datalayer.js
@@ -0,0 +1,51 @@
+digitalData = {};
+
+
+digitalData =
+{
+    "userAction": {
+        "actionName": "form-submit"
+    },
+    "page": {
+        "pageInfo": {
+            "pageName": "Home",
+            "pageID": "en",
+            "pageUrl": "http://local.dev.lifeway.com:4502/content/lifeway/us/en.html",
+            "domain": "local.dev.lifeway.com",
+            "tags": "",
+            "tabType": "",
+            "tabName": ""
+        },
+        "category": {
+            "pageType": "Base Page",
+            "primaryCategory": "",
+            "subCategory1": "n/a",
+            "subCategory2": "n/a",
+            "subCategory3": "n/a"
+        },
+        "formandFile": {
+            "fileID": "",
+            "formStart": "form-start",
+            "lastField": "form-end",
+            "formComplete": "",
+            "register": "",
+            "formLabel": "",
+            "fileType": ""
+        }
+    },
+    "user": {
+        "profile": {
+            "profileInfo": {
+                "profileID": "ptoc lw03@xumak.com",
+                "marketingCloudID": "",
+                "socialID": "",
+                "signIn": "",
+                "accountID": ""
+            }
+        }
+    },
+    "search": {},
+    "results": {},
+    "product": [],
+    "cart": {}
+}
