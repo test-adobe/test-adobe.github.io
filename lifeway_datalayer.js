f2d9d50fffd91d97a88668e235ade9e19668d5d9
test-adobe <hravi@adobe.com>
2017-06-11 23:07:38 -0700

    Create lifeway_datalayer.js

--- lifeway_datalayer.js
+++ lifeway_datalayer.js
@@ -44,8 +44,55 @@ digitalData ={
             }
         }
     },
-    "search": {},
-    "results": {},
+     "search": {
+        "internalSearchType": "",
+        "internalSearchTerm": "ring",
+        "internalSearchResults": "76",
+        "internalSearchCategory": "PRODUCT",
+        "internalSearchSubcategory": "",
+        "internalSearchFacets": {
+            "facet0": "Publisher:WestBow Press,WestBow Press"
+        }
+    },
+    "results": {
+        "searchResults": "76",
+        "learnResults": "",
+        "productResults": "76",
+        "eventResults": "",
+        "serviceResults": "",
+        "engageResults": "",
+        "typeAheadkeyStrokes": ["rin", "ring", "Three Ring Circus: Life as a Missionary Kid in a Family of 11", "WALKING IN THE RINGS OF GOD"]
+    },
     "product": [],
-    "cart": {}
+    "cart": {
+	        "price": {
+	            "orderID": "",
+	            "listPrice": "",
+	            "quantity": "",
+	            "shippingMethod": "",
+	            "paymentMethod": "",
+	            "basePrice": 47.98,
+	            "shipping": 0,
+	            "currency": "USD",
+	            "taxTotal": 0,
+	            "cartTotal": 47.98,
+	            "billing": "",
+	            "discount": 0,
+	            "cartLocation": "",
+	            "cartAction": "",
+	            "isFirstCart": ""
+	        },
+	        "item": [{
+	            "productInfo": {
+	                "productID": "001280066",
+	                "productName": "HCSB® Comprehensive Concordance",
+	                "sku": "001280066",
+	                "childSKU": "",
+	                "quantity": 2,
+	                "price": {
+	                    "basePrice": 29.99,
+	                    "currency": "USD"
+	                }
+	            }
+	        }]
 }
