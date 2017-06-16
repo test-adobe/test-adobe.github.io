b991aa4412080ba73525cd61ecd7871950af6ff6
test-adobe <hravi@adobe.com>
2017-06-16 11:28:02 -0700

    Create newsmax_datalayer.js

---
+++ newsmax_datalayer.js
@@ -0,0 +1,64 @@
+digitalData ={}
+
+digitalData ={
+    
+    userAction: {
+        actionName: ""
+    },
+
+    tab: {
+    	type: "",
+    	name:""
+    }
+
+    page: {
+        pageInfo: {
+            pageName: "",
+            PrimaryCategory: "",
+            subCategory1: "",
+            subCategory2: "",
+            pageType: "",
+            reportSuiteID:"",
+            status:""
+        	},
+
+        content: {
+        	contentType:"",
+        	contentID:"",
+        	contentName:""
+        	},
+
+        search: {
+        	onsiteSearchResults:"",
+        	onsiteSearchTerm:""
+        	},
+
+        social: {
+        	socialName: "",
+        	socialAction: ""
+        	},
+
+        },
+
+    transaction: {
+    		orderID: "",
+    		revenue:""
+    },
+
+    item:[{
+
+    	productInfo: {
+    		productID: "",
+    		productName:"",
+    		sku:"",
+    		units:"",
+    		price:""
+
+    	},
+
+
+    }],
+
+ }
+
+
